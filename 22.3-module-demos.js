f06c49356a5c8cb64b1088438c3d8ad10cb06859
Jae Kim <jckim81@yahoo.com>
2020-06-18 18:33:56 -0400

    modified 22.3-module-demos.js

--- 22.3-module-demos.js
+++ 22.3-module-demos.js
@@ -133,3 +133,83 @@ console.log(g);
 console.log('----------------------------');
 
 
+/* ------------------------------------------------------------- */
+
+//Polynomial time O(n^k)
+
+function hasDuplicates(array) {
+    let ticks = 0, result = false;
+    for (let i = 0; i < array.length; i++) {
+        ticks++;
+        for (let j = 0; j < array.length; j++) {
+            ticks++;
+            if (array[i] === array[j] && i !== j) {
+                result = true;
+            }
+        }
+    }
+    return {
+        result: result,
+        ticks: ticks
+    };
+}
+
+const h = hasDuplicates([1, 2, 2]);
+const i = hasDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 9]);
+const j = hasDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 99]);
+
+console.log('----------------------------');
+console.log('Polynomial time O(n^k)')
+console.log(h);
+console.log(i);
+console.log(j);
+console.log('----------------------------');
+
+/* ------------------------------------------------------------- */
+
+//Exponential time O(2^n)
+
+/* (e.g.)
+               *
+              * *
+            * * * *
+        * * * * * * * *
+* * * * * * * * * * * * * * * *
+
+*/
+function countTriangle(layers) {
+    let ticks = 1;
+    let count = 0; // the number of dots we've counted so far
+    let layer = 0; // the current layer we're on
+    let lastLayer = 1; // the number of dots we counted in the previous layer
+    while (layer < layers) {
+        ticks++;
+        let newLayer = 0; // the number of dots we've counted so far in the current layer
+        for (let i = 0; i < lastLayer; i++) {
+            ticks++;
+            newLayer += 2;
+        }
+        lastLayer = newLayer;
+        count += lastLayer;
+        layer++;
+    }
+    return {
+        result: count,
+        ticks: ticks
+    };
+}
+
+const k = countTriangle(2);
+const l = countTriangle(4);
+const m = countTriangle(16);
+
+console.log('----------------------------');
+console.log('Exponential time O(2^n)')
+console.log(k);
+console.log(l);
+console.log(m);
+console.log('----------------------------');
+
+
+
+
