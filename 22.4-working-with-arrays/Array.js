e675132d662b09471e221d7b34de3b87d0ff52f6
Jae Kim <jckim81@yahoo.com>
2020-06-24 10:43:36 -0400

    modified Array.js

--- 22.4-working-with-arrays/Array.js
+++ 22.4-working-with-arrays/Array.js
@@ -1,10 +1,75 @@
 const memory = require('./memory')
 
 class Array {
-    constructor(){
-        this.length = 0
-        this.ptr = memory.allocate(this.length)
+    constructor() {
+        this.length = 0;
+        this._capacity = 0;
+        this.ptr = memory.allocate(this.length);
     }
+
+    push(value) {
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO);
+        }
+
+        memory.set(this.ptr + this.length, value);
+        this.length++;
+    }
+
+    _resize(size) {
+        const oldPtr = this.ptr;
+        this.ptr = memory.allocate(size);
+        if (this.ptr === null) {
+            throw new Error('Out of memory');
+        }
+        memory.copy(this.ptr, oldPtr, this.length);
+        memory.free(oldPtr);
+        this._capacity = size;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        return memory.get(this.ptr + index);
+    }
+
+    pop() {
+        if (this.length == 0) {
+            throw new Error('Index error');
+        }
+        const value = memory.get(this.ptr + this.length - 1);
+        this.length--;
+        return value;
+    }
+
+    insert(index, value) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO);
+        }
+
+        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+        memory.set(this.ptr + index, value);
+        this.length++;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+        this.length--;
+    }
+
+
+
 }
+
+
+Array.SIZE_RATIO = 3;
 
 
