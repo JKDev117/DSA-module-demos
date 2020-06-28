b5792498155fe1bd6d57eed25f76285ec30d086d
Jae Kim <jckim81@yahoo.com>
2020-06-28 15:49:27 -0400

    added new file 22.5-working-with-linked-lists.js

---
+++ 22.5-working-with-linked-lists.js
@@ -0,0 +1,81 @@
+class _Node { //underscore _ indicates a private class
+    constructor(value, next){
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null; //head to indicate beginning of the list; the head always contains the 1st node
+    }
+
+    insertFirst(item){
+        this.head = new _Node(item, this.head)
+    }
+
+    insertLast(item){
+        if(this.head === null){ //the beginning of the list is indicated by head
+            this.insertFirst(item)
+        } else {
+            let tempNode = this.head;
+            while(tempNode.next !== null){
+                tempNode = tempNode.next;
+            }
+            tempNode.next = new _Node(item, null);
+        }
+    }
+
+    find(item) { 
+        // Start at the head
+        let currNode = this.head;
+        // If the list is empty
+        if (!this.head) {
+            return null;
+        }
+        // Check for the item 
+        while (currNode.value !== item) {
+            /* Return null if it's the end of the list 
+               and the item is not on the list */
+            if (currNode.next === null) {
+                return null;
+            }
+            else {
+                // Otherwise, keep looking 
+                currNode = currNode.next;
+            }
+        }
+        // Found it
+        return currNode;
+    }
+
+    remove(item){ 
+        // If the list is empty
+        if (!this.head) {
+            return null;
+        }
+        // If the node to be removed is head, make the next node head
+        if (this.head.value === item) {
+            this.head = this.head.next;
+            return;
+        }
+        // Start at the head
+        let currNode = this.head;
+        // Keep track of previous
+        let previousNode = this.head;
+
+        while ((currNode !== null) && (currNode.value !== item)) {
+            // Save the previous node 
+            previousNode = currNode;
+            currNode = currNode.next;
+        }
+        if (currNode === null) {
+            console.log('Item not found');
+            return;
+        }
+        previousNode.next = currNode.next;
+    }
+
+}
+
+
